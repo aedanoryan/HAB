3a6a2c8a7525457e5f3f9c35a17c46c37da1c3a0
aedanoryan <aedanryan@gmail.com>
2026-03-13 11:17:13 -0400

    Update testing.py

---
+++ testing.py
@@ -0,0 +1,18 @@
+
+from pathlib import Path
+from collections import Counter
+
+def count_extensions(parsed_path: Path):
+    folder = Path(parsed_path)
+
+    ext_dict = Counter(
+        i.suffix.lower()
+        for i in folder.rglob("*")
+        if i.is_file()
+    )
+
+    return ext_dict
+my_path = Path( "C:\Users\aedan\Desktop\Maxone backup")
+ext_dict =count_extensions(my_path)
+for i in ext_dict:
+        print(f"There are {ext_dict[i]} files with the extension {i}")
